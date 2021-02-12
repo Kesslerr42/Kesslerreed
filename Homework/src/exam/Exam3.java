a8ad665b8cd306be9d61d0366211ea009cd97747
Kesslerr <kesslerr@student.swosu.edu>
2021-02-12 09:08:51 -0600

    Push

---
+++ Homework/src/exam/Exam3.java
@@ -0,0 +1,32 @@
+package exam;
+
+public class Exam3 {
+
+	public static void main(String[] args) {
+			int range = 10000;
+			int primes = 0;
+			for (int check = 2; check < range; check++)
+			{
+				
+				if(isPrime(check))
+				{
+				
+					primes++;
+									}
+			}
+			System.out.println("We have found " + primes + " primes."); 
+		}
+
+		private static boolean isPrime(int check) {
+			boolean isPrime = true;
+			for(int possibleFactor = 2; possibleFactor < check; possibleFactor++)
+			{
+				if(check % possibleFactor == 0)
+				{
+					isPrime = false;
+					return isPrime;
+				}
+			}
+			return isPrime;
+		}
+	}
