bd71b75cde188a1a72a08dceecd75d54320685d3
hunte <hunte@10.30.98.28>
2021-03-01 08:31:51 -0600

    More

---
+++ Homework/src/chapter12/WithoutException.java
@@ -0,0 +1,50 @@
+package chapter12;
+
+public class WithoutException {
+
+	public static void main(String[] args) throws Exception {
+		// Check number of strings passed
+		if (args.length != 3) {
+			System.out.println(
+				"Usage: java Calculator operand1 operator operand2");
+			System.exit(0);
+		}
+		
+	
+		testDigit(args[0]);
+		testDigit(args[2]);
+
+		// The result of the operation
+		int result = 0;
+
+		// Determine the operator
+		switch (args[1].charAt(0)) {
+			case '+' : result = Integer.parseInt(args[0]) +
+									  Integer.parseInt(args[2]);
+						  break;
+			case '-' : result = Integer.parseInt(args[0]) -
+									  Integer.parseInt(args[2]);
+						  break;
+			case '.' : result = Integer.parseInt(args[0]) *
+									  Integer.parseInt(args[2]);
+						  break;
+			case '/' : result = Integer.parseInt(args[0]) /
+									  Integer.parseInt(args[2]);
+						  break;
+		}
+
+		// Display result
+		System.out.println(args[0] + ' ' + args[1] + ' ' + args[2]
+			+ " = " + result);
+	}
+
+	
+	private static void testDigit(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			if (!Character.isDigit(str.charAt(i))) {
+				System.out.println("Wrong Input: " + str);
+				System.exit(0);
+			}
+		}
+	}
+}
